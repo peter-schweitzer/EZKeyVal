93239a152d66da82a24181e006a25aa580a97380
peter-schweitzer <peter.schweitzer.official@gmail.com>
2022-06-08 10:45:33 +0200

    add persistance

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-import { App, buildRes, serveFromFS } from './EZServer/EZServer.js';
+import { readFileSync, writeFileSync } from 'fs';
 
+import { App, buildRes, serveFromFS } from './EZServer/EZServer.js';
 import { getBodyJSON } from './EZServer/endpoints/REST.js';
 
 const LOG = console.log;
 
 const app = new App('1337');
 
+let data_from_FS = '{}';
+try {
+  data_from_FS = readFileSync('./data.json', { encoding: 'utf8' });
+} catch (e) {
+  console.warn('Error while reading data.json', e);
+}
+
+let values = JSON.parse(data_from_FS);
+
+/** @returns {void} */
+function saveToFS() {
+  writeFileSync('./data.json', JSON.stringify(values), { encoding: 'utf8' });
+}
+
 app.addResolver('/', (req, res) => {
   serveFromFS('./EZServer/html/home.html', res);
 });
-
-let values = {};
 
 app.rest.get('/api', (req, res) => {
   LOG('\n> GET:\n-------');
@@ -40,6 +53,7 @@ app.rest.put('/api', async (req, res) => {
   if ((val = json.value) !== undefined) ((values[key] = val) || true) && LOG('new val:', val);
   else !(http_code = 0) && LOG('VALUE UNCHANGED');
 
+  saveToFS();
+
   res.writeHead(http_code).end();
 });
-
